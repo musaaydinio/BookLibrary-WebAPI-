94609ba8ac21bbcec22ba27579833abf9474d816
Musa Aydin <mfmy982@gmail.com>
2026-06-10 20:11:22 +0300

    chore: remove unrelated projects from repository tracking

--- bookdemo/Models/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace bookdemo.Models
-{
-    public class Book
-    {
-        public int Id { get; set; }
-        public required string Title { get; set; }
-        public decimal Price { get; set; }
-    }
-}
--- helloWEBaPI/NewFolder3/ResponModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace helloWEBaPI.NewFolder3
-{
-    public class ResponModel
-    {
-        public int HttpStatus { get; set; }
-        public String Message { get; set; }
-    }
-}
