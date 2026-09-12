56294752adad48da99c1be41c99bf7cf89e1aad8
Musa Aydin <mfmy982@gmail.com>
2026-09-12 20:23:11 +0300

    Swagger Test

--- bsStoreApp/Entities/DataTranferObjcets/UserForResgistration.cs
+++ bsStoreApp/Entities/DataTranferObjcets/UserForResgistration.cs
@@ -1,46 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace Entities.DataTranferObjcets
 {
-    [XmlRoot("UserForResgistrationDto")]
-    public class UserForResgistrationDto // record yerine class yapıldı
+    public record UserForResgistrationDto
     {
-        /// <example>Musa</example>
-        [XmlElement("firstName")]
         public string? FirstName { get; set; }
-
-        /// <example>Aydın</example>
-        [XmlElement("lastName")]
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Username is required.")]
-        /// <example>musaaydin</example>
-        [XmlElement("userName")]
         public string? UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
-        /// <example>Password123!</example>
-        [XmlElement("password")]
         public string? Password { get; set; }
 
-        /// <example>musa@example.com</example>
-        [XmlElement("email")]
         public string? Email { get; set; }
-
-        /// <example>5551234567</example>
-        [XmlElement("phoneNumber")]
         public string? PhoneNumber { get; set; }
 
-        /// <example>User</example>
-        [XmlArray("roles")]
-        [XmlArrayItem("string")]
-        public List<string>? Roles { get; set; } // Dizi yapısı Swagger XML formatına tam hizalandı
+        public ICollection<string>? Roles { get; set; }
     }
 }
 
