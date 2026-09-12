3c49b9d56807ee6a2674d497948c02c530bf23ac
Musa Aydin <mfmy982@gmail.com>
2026-09-12 20:01:01 +0300

    Swagger Test

--- bsStoreApp/Entities/DataTranferObjcets/UserForResgistration.cs
+++ bsStoreApp/Entities/DataTranferObjcets/UserForResgistration.cs
@@ -11,28 +11,35 @@ namespace Entities.DataTranferObjcets
     [XmlRoot("UserForResgistrationDto")]
     public record UserForResgistrationDto
     {
+        /// <example>Musa</example>
         [XmlElement("firstName")]
         public string? FirstName { get; set; }
 
+        /// <example>Aydın</example>
         [XmlElement("lastName")]
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Username is required.")]
+        /// <example>musaaydin</example>
         [XmlElement("userName")]
         public string? UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
+        /// <example>Password123!</example>
         [XmlElement("password")]
         public string? Password { get; set; }
 
+        /// <example>musa@example.com</example>
         [XmlElement("email")]
         public string? Email { get; set; }
 
+        /// <example>5551234567</example>
         [XmlElement("phoneNumber")]
         public string? PhoneNumber { get; set; }
 
+        /// <example>User</example>
         [XmlElement("roles")]
-        public ICollection<string>? Roles { get; set; }
+        public List<string>? Roles { get; set; }
     }
 }
 
