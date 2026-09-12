51331ac615c40ee8e55c5ec998e5085c4d68d93d
Musa Aydin <mfmy982@gmail.com>
2026-09-12 18:12:28 +0300

    resgiter

--- bsStoreApp/Entities/DataTranferObjcets/UserForResgistration.cs
+++ bsStoreApp/Entities/DataTranferObjcets/UserForResgistration.cs
@@ -9,17 +9,24 @@ namespace Entities.DataTranferObjcets
 {
     public record UserForResgistrationDto
     {
+        /// <example>John</example>
         public string? FirstName { get; set; }
+        /// <example>Doe</example>
         public string? LastName { get; set; }
 
+        /// <example>johndoe</example>
         [Required(ErrorMessage ="Username is required.")]
         public string? UserName { get; set; }
 
+        /// <example>Password123!</example>
         [Required(ErrorMessage = "Password is required.")]
         public string? Password { get; set; }
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
 
+        /// <example>john.doe@example.com</example>
+        public string? Email { get; set; }
+        /// <example>5551234567</example>
+        public string? PhoneNumber { get; set; }
+        /// <example>["User"]</example>
         public ICollection<string>? Roles { get; set; }
 
     }
